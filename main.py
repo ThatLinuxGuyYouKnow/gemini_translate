8788e1e3513f667a80688fca2e8fb1921949e9df
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-04-16 21:41:09 +0100

    feature: stated POST as method

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
  
 CORS(app)  
 
-@app.route('/translate')
+@app.route('/translate', methods = ['POST'])
 def translation_route():
     status,response = translateText(request)
     if status:
