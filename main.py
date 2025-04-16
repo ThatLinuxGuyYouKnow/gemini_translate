ac796d8a7d8e31d36ae4f9465fc86928f951b2c0
ThatLinuxGuyYouKnow <arocket04@gmail.com>
2025-04-16 19:13:37 +0100

    chore: crafted prompt

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+from routes.translate import translateText
+
 app = Flask(__name__)
  
 CORS(app)  
+
+@app.route('/translate')
+def translation_route():
+    translateText(request)
--- routes/translate.py
+++ routes/translate.py
@@ -1,6 +1,13 @@
 import requests
 
-def translateText():
+def translateText(request):
+    data = request.get_json()
+    target_language = data.get("target_language")
+    original_language = data.get("original_language")
+    text_to_convert = data.get("text")
+    api_key = data.get("api_key")
+    prompt: str = f""" 
+Convert the text {text_to_convert} from {original_language} to {target_language}. If the"""
     payload = {
           "system_instruction": {
                 "parts": [
@@ -18,8 +25,9 @@ def translateText():
                         }
                     ]
                 }
-            ]
+            ],
+            "generationConfig":"application/json"
     }
-    response = requests.post(f"https://generativelanguage.googleapis.com/v1beta/models/{CONFIG.model}:generateContent?key={apikey}",
+    response = requests.post(f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash/generateContent?key={api_key}",
         headers={'Content-Type': "application/json"},
         data=json.dumps(payload))
